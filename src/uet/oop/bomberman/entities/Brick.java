8b2bfc0886f17d21a77fc3b34813775c5c95f9f2
Nhanhhhh <nhatnguyenb25@gmail.com>
2023-04-08 20:53:49 +0700

    map_init
    khởi tạo balloom, oneal, item, map

---
+++ src/uet/oop/bomberman/entities/Brick.java
@@ -0,0 +1,17 @@
+package uet.oop.bomberman.entities;
+
+import javafx.scene.image.Image;
+
+import java.awt.*;
+
+public class Brick extends Entity {
+
+    public Brick(int x, int y, Image img) {
+        super(x, y, img);
+    }
+
+    @Override
+    public void update() {
+
+    }
+}
---
+++ src/uet/oop/bomberman/entities/Character/Oneal.java
@@ -0,0 +1,15 @@
+package uet.oop.bomberman.entities.Character;
+
+import javafx.scene.image.Image;
+import uet.oop.bomberman.entities.Entity;
+
+public class Oneal extends Entity {
+    public Oneal(int x, int y, Image img) {
+        super(x, y, img);
+    }
+
+    @Override
+    public void update() {
+
+    }
+}
